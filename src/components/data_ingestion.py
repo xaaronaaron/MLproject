a1de95755e25b02380d876d80b581354e713f846
xaaronaaron <aarjacob47@gmail.com>
2023-08-21 13:24:36 +0530

    test and train csv

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,5 @@
 import os
 import sys 
-#sys.path.insert(0, r'C:\Users\aarja\OneDrive\Documents\Aaron CMRIT\Projects\MLproject\src')
 from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
@@ -32,8 +31,8 @@ class DataIngestion :
             
             train_set, test_set = train_test_split(df, test_size = 0.2, random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path, Index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, Index=False, header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
             logging.info("Ingestion of the data is complete")
 
             return(
